b622755dc0cebc7d8a083837484873651e56e495
Zheng Tang <oscartang1120@gmail.com>
2019-04-25 02:04:30 -0700

    1

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import time
 if __name__ == '__main__':
     random.seed(1)
     start = time.time()
-    lang, train_set = prepare_jldata("snli_1.0/snli_1.0_train.jsonl")
+    lang, train_set = prepare_jldata("snli_1.0/snli_1.0_dev.jsonl")
     lang, dev_set = prepare_jldata("snli_1.0/snli_1.0_dev.jsonl", lang)
     embeds = load_embeddings("glove.6B.200d.txt", lang.words)
     classifier = Classifier(200, embeds, len(lang.words), 100, 48, 4)
@@ -27,13 +27,19 @@ if __name__ == '__main__':
             loss.backward()
             optimizer.step()
 
-        if (e + 0) % 10 == 0:
+        if e % 10 == 0:
             classifier.eval()
+            gls = list()
+            pls = list()
             for datapoint in dev_set:
                 premise = torch.LongTensor(datapoint["premise"])
                 hypothesis = torch.LongTensor(datapoint["hypothesis"])
                 gold_label = datapoint["gold_label"]
+                gls.append(gold_label)
                 predict = np.argmax(classifier(premise, hypothesis).data.numpy())
+                pls.append(predict)
+            res = np.array([pls[i]==gls[i] for i in range(len(pls))])
+            acc = float(np.sum(res)/len(gls))
             done = time.time()
             elapsed = done - start
             print("Epoch %d: used time: %d"%(e, elapsed))
